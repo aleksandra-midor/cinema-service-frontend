29cfad912bd2c28404a48541f82bc6e1e4e9f2c8
octane\ola <ola_midor@outlook.com>
2020-10-25 21:10:22 +0100

    initial commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.scss'
+
+function NavBar () {
+  return (
+    <header className="NavBar">
+      <p className="NavBar_Logo">Cinema Kryszak</p>
+      <ul className="NavBar_List">
+        <li>movies</li>
+        <li>find a theatre</li>
+      </ul>
+    </header>
+  )
+}
+
+export default NavBar;
---
+++ src/Pages/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainPage() {
+  return (
+    <main className="MainPage">
+    <header className="MainPage_Header">
+    <h1>Cinema tickets</h1>
+  </header>
+  </main>
+  )
+}
+
+export default MainPage;
