8acff042e314c9b58fca6f16e53686bd90a3b116
octane\ola <ola_midor@outlook.com>
2020-10-31 14:20:40 +0100

    displaying list of available cinemas

--- src/Components/CinemaSelection/CinemaSelection.js
+++ src/Components/CinemaSelection/CinemaSelection.js
@@ -1,20 +1,20 @@
-import React, {
-  //  useContext,
-  useState,
-} from "react";
-// import AppContext from "../../store/context";
+import React, { useContext, useState } from "react";
+import AppContext from "../../store/context";
 import Modal from "../Modal/Modal";
 
 function CinemaSelection() {
-  // const { state } = useContext(AppContext);
+  const { state } = useContext(AppContext);
+  const { cinemas } = state;
   const [modalVisible, setModalVisible] = useState(true);
-
-  // console.log("::::::::::::::::::::::::::::::::::::::::::::::::::", state);
+  const displayCinemas = cinemas.map((cinema) => {
+    return <p key={cinema.name}>{cinema.city}</p>;
+  });
+  console.log("::::::::::::::::::::::::::::::::::::::::::::::::::", state);
   return (
     <div>
       Select your cinema
       <Modal visible={modalVisible} onClickOk={() => setModalVisible(false)}>
-        ***** ***
+        {displayCinemas}
       </Modal>
     </div>
   );
