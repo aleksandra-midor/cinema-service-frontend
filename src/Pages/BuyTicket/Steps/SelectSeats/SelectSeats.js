ea0307c2364948f206453ba3a0bac91c09d148d7
octane\ola <ola_midor@outlook.com>
2020-11-08 21:01:23 +0100

    SelectSeats component fetches unavailable seats

--- src/Pages/BuyTicket/Steps/SelectSeats/SelectSeats.js
+++ src/Pages/BuyTicket/Steps/SelectSeats/SelectSeats.js
@@ -1,14 +1,28 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
 import AppContext from "../../../../store/context";
 import Button from "../../../../Components/Button/Button";
 
 const SelectSeats = (props) => {
   const { state, dispatch } = useContext(AppContext);
   const [selectedSeats, setSelectedSeats] = useState([]);
+  const [unavailableSeats, setUnavailableSeats] = useState([]);
 
   const setTicket = (data) => {
     dispatch({ type: "setTicket", data });
   };
+
+  useEffect(() => {
+    const fetchUnavailableSeats = async () => {
+      const result = await axios({
+        method: "get",
+        url: `http://localhost:5001/api/v1/bookedSeats/${state.selectedCinema._id}/${state.ticket.movieId}/${state.ticket.date}/${state.ticket.hour}`,
+      });
+      console.log("..................", state);
+      setUnavailableSeats(result.data);
+    };
+    fetchUnavailableSeats();
+  }, []);
 
   const handleSelectedSeats = (el) => {
     console.log(el);
@@ -26,7 +40,7 @@ const SelectSeats = (props) => {
     return state.selectedCinema.ticketPrice * selectedSeats.length;
   };
 
-  const nrOfSeats = 20;
+  const nrOfSeats = 25;
   const allSeats = [];
   for (let i = 1; i <= nrOfSeats; i += 1) {
     allSeats.push(i);
@@ -41,6 +55,7 @@ const SelectSeats = (props) => {
           <>
             <input
               type="checkbox"
+              disabled={unavailableSeats.find((el) => el === seat)}
               id={`seat-${seat}`}
               value={seat}
               onChange={() => handleSelectedSeats(seat)}
