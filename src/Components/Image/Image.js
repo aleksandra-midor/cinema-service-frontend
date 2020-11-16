00c65d427cd043f800e7394543d22adac45a1d47
octane\ola <ola_midor@outlook.com>
2020-11-16 07:44:24 +0100

    cloudinary image resize to 1200pix

--- src/Components/Image/Image.js
+++ src/Components/Image/Image.js
@@ -5,7 +5,7 @@ const Image = (props) => {
   return (
     <div className="Image">
       <img
-        src={`https://res.cloudinary.com/midor/image/upload/v1605368758/cinemaparadiso/${props.imageName}.webp`}
+        src={`https://res.cloudinary.com/midor/image/upload/c_scale,q_90,w_1200/v1605368758/cinemaparadiso/${props.imageName}.webp`}
         alt={props.title}
       />
     </div>
