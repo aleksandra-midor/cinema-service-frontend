0cacea82fcc7e7516239deb9be5c27cb16cad4e2
octane\ola <ola_midor@outlook.com>
2020-12-02 23:51:46 +0100

    CheckoutFrom test added

---
+++ src/Components/CheckoutForm/__tests__/CheckoutForm.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, act, waitFor } from "@testing-library/react";
+import { Elements } from "@stripe/react-stripe-js";
+import { WithProvider } from "../../../mockTestData/WithProvider";
+import CheckoutForm from "../CheckoutForm";
+
+// From https://github.com/i18next/react-i18next/issues/876
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({ t: (key) => key }),
+  getI18n: () => {
+    return { language: "sv" };
+  },
+}));
+
+async function renderWrapper() {
+  let component;
+
+  act(() => {
+    component = render(
+      <WithProvider>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      </WithProvider>
+    );
+  });
+  return component;
+}
+
+describe("CheckoutForm testing", () => {
+  beforeEach(() => {});
+
+  test("component renders", async () => {
+    const { getByTestId } = await renderWrapper();
+    const page = await waitFor(() => getByTestId("CheckoutForm"));
+    expect(page).toBeInTheDocument();
+  });
+});
